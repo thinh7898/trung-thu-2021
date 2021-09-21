bdc3ebb068664d8d59d6e6adb3415bb5caeb4f04
thinh7898 <nd.thinh7898@gmail.com>
2021-09-21 09:51:02 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chào bạn Ly ! 🖐🖐🖐',
+    introDesc: `T có cái này hay cực :v, nhấn vô để biết nhé !!!`,
+    btnIntro: 'OK',
+    title: 'Bạn thích ăn bánh trung thu ko :)) ? ',
+    desc: 'trả lời thật lòng nha, ko đc out đó :> ',
+    btnYes: 'Ăn chán phèo à ',
+    btnNo: 'Có, đương nhiên là thích ăn rồi 💖💖💖 ! ',
+    question:'Đùa tý thôi, vậy thích ăn vị gì nào 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! ',
+    reply: 'Vị nào cũng thích 😊😊',
+    mess: 'thế tối mai t mang đến nhé :3',
+    messDesc: '💬💬 Nhắn tin luôn nha b. 💬💬',
+    btnAccept: 'Ok lun :v',
+    messLink: 'https://www.facebook.com/profile.php?id=100014003564286'
+}
